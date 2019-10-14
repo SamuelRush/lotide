d00f5b8f0afed9bfd95b1ddb5d4c269772d088e0
Sam <samuelrush@gmail.com>
2019-10-14 21:46:36 +0000

    Created replacement to the console.assert function

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  let message = "";
+  if (actual === expected) {
+    message = "🤙🤙🤙 Assertion Passed: " + actual + " === " + expected;
+  } else {
+    message = "💩💩💩 Assertion Failed: " + actual + " !== " + expected;
+  }
+  console.log(message);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 1.1);
