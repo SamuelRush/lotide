9c633b0264e57ba47c685dd6bf4c7eaf0d65acda
Sam <samuelrush@gmail.com>
2019-10-15 23:54:48 +0000

    added middle function

---
+++ middle.js
@@ -0,0 +1,21 @@
+const middle = function(array){
+  let newArray = [];
+  let arrayLength = (array.length)/2;
+
+  if(array.length > 2){
+    if((arrayLength % 1) !== 0) {
+      newArray.push(array[(arrayLength + 0.5 - 1)]);
+    } else {
+      newArray.push(array[arrayLength - 1]);
+      newArray.push(array[(arrayLength)]);
+    }
+  }
+console.log(newArray);
+}
+
+middle([1]) // => []
+middle([1, 2]) // => []
+middle([1, 2, 3]) // => [2]
+middle([1, 2, 3, 4, 5]) // => [3]
+middle([1, 2, 3, 4]) // => [2, 3]
+middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
