4d4857eb537f89b5123b7abc00b21ac974d50336
Sam <samuelrush@gmail.com>
2019-10-15 23:35:16 +0000

    Added assertArrayEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+const eqArrays = function(one,two){
+  let answer = true
+  let message = "";
+
+  for(x = 0; x < one.length; x++) {
+    if (one[x] !== two[x]) answer = false;
+  }
+  if (answer) {
+    message = `🤙🤙🤙 Assertion Passed: ${one} === ${two}`;
+  } else {
+    message = `💩💩💩 Assertion Failed: ${one} !== ${two}`;
+  }
+  console.log(message);
+}
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+
+
