ba3ab2c4212ad2124785636c4815b093846a4aaa
Sam <samuelrush@gmail.com>
2019-10-16 00:38:26 +0000

    Add flatten function to get rid of arrays with nested arrays

---
+++ flatten.js
@@ -0,0 +1,15 @@
+const flatten = function(array){
+  newArray = [];
+  for (let x = 0; x < array.length; x++){
+    if(Array.isArray(array[x])){
+      for (let y = 0; y < array[x].length; y++){
+        newArray.push(array[x][y]);
+      }
+    } else {
+      newArray.push(array[x]);
+    }
+  }
+  console.log(newArray);
+}
+
+flatten([1, 2, [3, 4], 5, [6]]) // => [1, 2, 3, 4, 5, 6]
