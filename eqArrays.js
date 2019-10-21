634f5e5e18427eb103e8a76ec8276e433a9e874d
Sam <samuelrush@gmail.com>
2019-10-21 16:14:04 +0000

    replaces eqArray test assert

--- eqArrays.js
+++ eqArrays.js
@@ -1,6 +1,6 @@
 // FUNCTION IMPLEMENTATION
 
-const assertEqual = function(actual, expected) {
+/*const assertEqual = function(actual, expected) {
   let message = "";
   if (actual === expected) {
     message = `🤙🤙🤙 Assertion Passed: ${actual} === ${expected}`;
@@ -8,7 +8,7 @@ const assertEqual = function(actual, expected) {
     message = `💩💩💩 Assertion Failed: ${actual} !== ${expected}`;
   }
   console.log(message);
-};
+};*/
 
 const eqArrays = function(one,two){
   for(x = 0; x < one.length; x++) {
@@ -18,9 +18,11 @@ const eqArrays = function(one,two){
 }
 
 // TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true) // => true
+/*assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true) // => true
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false*/
+
+module.exports = eqArrays;
 
