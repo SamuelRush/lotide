329562d4b3ada72a6bb3298e6237aa529ff9b9c4
Sam <samuelrush@gmail.com>
2019-10-21 22:38:00 +0000

    Added chai mocha asserts

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,8 +1,22 @@
-const assertEqual = require('../assertEqual');
+/*const assertEqual = require('../assertEqual');
 const assertArraysEqual = require("../assertArraysEqual");
 
 assertEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]),true) // => true
 assertEqual(assertArraysEqual([1, 2, 3], [3, 2, 1]),false) // => false
 
 assertEqual(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]),true) // => true
-assertEqual(assertArraysEqual(["1", "2", "3"], ["1", "2", 3]),false) // => false
+assertEqual(assertArraysEqual(["1", "2", "3"], ["1", "2", 3]),false) // => false*/
+
+const expect = require("chai").expect;
+const assertArraysEqual = require("../assertArraysEqual");
+describe("#assertArrayEqual", () => {
+    it("should display true if the [1,2,3] is equal to [1,2,3]", () => {
+        expect(true).to.eql(assertArraysEqual([1, 2, 3], [1, 2, 3]))
+    });
+    it("should display false if the [1,2,3] is not equal to [3,2,1]", () => {
+        expect(false).to.eql(assertArraysEqual([1, 2, 3], [3, 2, 1]))
+    });
+    it("should display false if the ['1', '2', '3'] is not equal to ['1', 2, 3]", () => {
+        expect(false).to.eql(assertArraysEqual(["1", "2", "3"], ["1", 2, 3]))
+    });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,19 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require("chai").assert;
+const expect = require("chai").expect
+
+describe ("#tail", () =>{
+  it ("should return 6 when grabbing index 0 of [5,6,7]", () => {
+    assert.equal(tail([5,6,7])[0],6)
+  });
+  it ("should return [6,7] when given array [5,6,7]", () => {
+    expect(tail([5,6,7])).to.eql([6,7])
+  });
+});
+
+/*const assertEqual = require('../assertEqual');
 
 assertEqual(tail([5,6,7])[0], 6);
 assertEqual(tail(["Hello", "Lighthouse", "Labs"])[0], "Hello");
 assertEqual(tail([5,7])[0], 7);
-assertEqual(tail([])[0], "Hello");
+assertEqual(tail([])[0], "Hello");*/
